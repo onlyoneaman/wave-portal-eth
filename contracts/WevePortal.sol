4084f8fbd32d5af264b772d8f37a5f7ab6527803
Aman <2000.aman.sinha@gmail.com>
2022-03-09 11:54:39 +0530

    Cooldown in waves

--- contracts/WevePortal.sol
+++ contracts/WevePortal.sol
@@ -28,6 +28,13 @@ contract WavePortal {
      */
     Wave[] waves;
 
+
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;
+
     constructor() payable {
         console.log("Yo yo, I am a contract and I am smart");
 
@@ -40,6 +47,17 @@ contract WavePortal {
      * sends us from the frontend!
      */
     function wave(string memory _message) public {
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        // Update the current timestamp we have for the user
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s waved w/ message %s", msg.sender, _message);
 
@@ -47,7 +65,6 @@ contract WavePortal {
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
         seed = (block.difficulty + block.timestamp + seed) % 100;
-
 
         console.log("Random # generated: %d", seed);
 
